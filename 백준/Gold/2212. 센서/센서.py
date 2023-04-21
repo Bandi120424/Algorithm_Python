6b40e74d0e93bf76ec5d4ced7f68d03e7fe4f5de
N_Han <hny0309@gmail.com>
2023-04-21 09:07:57 +0900

    [Gold V] Title: 센서, Time: 48 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.py
+++ 백준/Gold/2212. 센서/센서.py
@@ -7,20 +7,15 @@ base_station_num = int(input())
 sensor_pos = sorted(list(map(int, input().split())))
 
 
-# def min_transmission_area():
-if base_station_num >= sensor_num:  # 각 센서에 집중국 설치
-    print(0)
-    sys.exit()
+def min_transmission_area():
+    if base_station_num >= sensor_num:  # 각 센서에 집중국 설치
+        return 0
 
-distance = []
-for i in range(1, sensor_num):
-    distance.append(sensor_pos[i] - sensor_pos[i-1])
+    distance = []
+    for i in range(1, sensor_num):
+        distance.append(sensor_pos[i] - sensor_pos[i-1])
 
-distance.sort(reverse=True)
-for _ in range(base_station_num-1):
-    distance.pop(0)
+    distance.sort(reverse=True)
+    return sum(distance[base_station_num-1:])
 
-print(sum(distance))
-
-
-# print(min_transmission_area())
+print(min_transmission_area())
