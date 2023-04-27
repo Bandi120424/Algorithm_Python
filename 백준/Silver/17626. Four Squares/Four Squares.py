d493bec225c0fc55872c3ea3a57b407fe3e062a1
N_Han <hny0309@gmail.com>
2023-04-28 08:37:30 +0900

    [Silver III] Title: Four Squares, Time: 252 ms, Memory: 115388 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+num = int(input())
+
+
+def is_square(num):
+    root_num = num**(0.5)
+    if root_num == int(root_num):
+        return True
+    return False
+
+
+lagrange_arr = [0] * (num+1)
+for i in range(1, num+1):
+    if is_square(i):
+        lagrange_arr[i] = 1
+        continue
+    lagrange_arr[i] = min(lagrange_arr[i-j**2]
+                          for j in range(1, int(i**(0.5))+1)) + 1
+
+print(lagrange_arr[num])
