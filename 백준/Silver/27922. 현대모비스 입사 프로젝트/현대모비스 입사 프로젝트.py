409db0cf87f7eb5ed6c3d27b015984641bff8da2
N_Han <hny0309@gmail.com>
2023-04-29 12:00:56 +0900

    [Silver III] Title: 현대모비스 입사 프로젝트, Time: 216 ms, Memory: 44388 KB -BaekjoonHub

--- 백준/Silver/27922. 현대모비스 입사 프로젝트/현대모비스 입사 프로젝트.py
+++ 백준/Silver/27922. 현대모비스 입사 프로젝트/현대모비스 입사 프로젝트.py
@@ -1,21 +1,29 @@
 import sys
-from itertools import combinations
 input = sys.stdin.readline
 
-total_courses, take_courses = map(int, input().split())
-lecture_info_sum1 = []
-lecture_info_sum2 = []
-lecture_info_sum3 = []
+def init_data():
+    total_courses, take_courses = map(int, input().split())
+    lecture_info_sum1 = []
+    lecture_info_sum2 = []
+    lecture_info_sum3 = []
 
-for _ in range(total_courses):
-    comm, algo, machine = map(int, input().split())
-    lecture_info_sum1.append(comm+algo)
-    lecture_info_sum2.append(comm+machine)
-    lecture_info_sum3.append(algo+machine)
-    
-ability_sum1 = sum(sorted(lecture_info_sum1, reverse = True)[:take_courses])
-ability_sum2 = sum(sorted(lecture_info_sum2, reverse = True)[:take_courses])
-ability_sum3 = sum(sorted(lecture_info_sum3, reverse = True)[:take_courses])
+    for _ in range(total_courses):
+        comm, algo, machine = map(int, input().split())
+        lecture_info_sum1.append(comm+algo)
+        lecture_info_sum2.append(comm+machine)
+        lecture_info_sum3.append(algo+machine)
+    return take_courses, lecture_info_sum1, lecture_info_sum2, lecture_info_sum3
 
-print(max(ability_sum1, ability_sum2, ability_sum3))
+def sorted_sum(lecture_list=None, take_courses: int = 0):
+    if lecture_list == None:
+        raise Exception("input is None")
+    sorted_lecture_list = sorted(lecture_list, reverse = True)
+    return sum(sorted_lecture_list[:take_courses])
 
+if __name__ == "__main__":
+    take_courses, lecture_info_sum1, lecture_info_sum2, lecture_info_sum3 = init_data()
+
+    ability_sum1 = sorted_sum(lecture_info_sum1, take_courses)
+    ability_sum2 = sorted_sum(lecture_info_sum2, take_courses)
+    ability_sum3 = sorted_sum(lecture_info_sum3, take_courses)
+    print(max(ability_sum1, ability_sum2, ability_sum3))
