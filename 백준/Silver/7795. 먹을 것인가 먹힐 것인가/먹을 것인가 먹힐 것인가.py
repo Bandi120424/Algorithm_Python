3191bf9e81d6becb68ba902273f8edb9bac2c687
N_Han <hny0309@gmail.com>
2023-04-30 11:06:46 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 116 ms, Memory: 33472 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -1,22 +1,18 @@
 import sys
 input = sys.stdin.readline
 
+def main():
+    n, m = map(int, input().split())
+    A = sorted(list(map(int, input().split())))
+    B = sorted(list(map(int, input().split())))
+    ret = 0
+    i = 0
+    for a in A:
+        while i < m and a > B[i]:
+            i += 1
+        ret += i
+    return ret
 
-def find_pair():
-    num_A, num_B = map(int, input().split())
-    set_A = list(map(int, input().split()))
-    set_B = sorted(list(map(int, input().split())), reverse=True)
-
-    ctn = 0
-    for ele_a in set_A:
-        for i in range(num_B):
-            if ele_a > set_B[i]:
-                ctn += num_B-i
-                break
-    return ctn
-
-
-if __name__ == "__main__":
-    test_case_num = int(input())
-    for _ in range(test_case_num):
-        print(find_pair())
+T = int(input())
+for _ in range(T):
+    print(main())
