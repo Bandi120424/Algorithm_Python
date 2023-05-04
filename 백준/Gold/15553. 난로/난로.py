876877bea451be27314e49ae3859cfae061d4d86
N_Han <hny0309@gmail.com>
2023-05-04 09:30:05 +0900

    [Gold V] Title: 난로, Time: 104 ms, Memory: 40140 KB -BaekjoonHub

--- 백준/Gold/15553. 난로/난로.py
+++ 백준/Gold/15553. 난로/난로.py
@@ -1,18 +1,26 @@
 import sys
 input = sys.stdin.readline
 
-total_friends, total_matches = map(int, input().split())
-arrival_time = [int(input()) for _ in range(total_friends)]
 
-if total_matches == 1:
-    print(arrival_time[-1]+1-arrival_time[0])
-    exit()
+def init_data():
+    total_friends, total_matches = map(int, input().split())
+    arrival_time = [int(input()) for _ in range(total_friends)]
+    return total_friends, total_matches, arrival_time
 
-time_diff = [arrival_time[i+1] - arrival_time[i]
-             for i in range(total_friends-1)]
 
-time_diff.sort()
-heating_time = sum(
-    time_diff[:total_friends - total_matches]) + (total_matches-1)
+def compute_diff(arrival_time, total_friends):
+    time_diff = [arrival_time[i+1] - arrival_time[i]
+                 for i in range(total_friends-1)]
+    return sorted(time_diff)
 
-print(heating_time+1)
+
+def heating_time(total_friends, total_matches, arrival_time):
+    if total_matches == 1:
+        return arrival_time[-1]+1-arrival_time[0]
+
+    time_diff = compute_diff(arrival_time, total_friends)
+    return sum(time_diff[:total_friends - total_matches]) + total_matches
+
+
+total_friends, total_matches, arrival_time = init_data()
+print(heating_time(total_friends, total_matches, arrival_time))
