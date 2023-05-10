1ccd1c67c7bd805d5acef10586ccbc0be9225538
N_Han <hny0309@gmail.com>
2023-05-10 09:19:15 +0900

    [Silver I] Title: 효율적인 해킹, Time: 11036 ms, Memory: 215508 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -1,15 +1,19 @@
 import sys
-from collections import deque, defaultdict
+from collections import deque
 input = sys.stdin.readline
 
-total_companies, total_relation = map(int, input().split())
-given_relation = [[] for _ in range(total_companies+1)]
-for _ in range(total_relation):
-    v1, v2 = map(int, input().split())
-    given_relation[v2].append(v1)  # v2 then v1
+
+def init_data():
+    total_companies, total_relation = map(int, input().split())
+    given_relation = [[] for _ in range(total_companies+1)]
+    for _ in range(total_relation):
+        v1, v2 = map(int, input().split())
+        given_relation[v2].append(v1)  # v2 then v1
+
+    return total_companies, given_relation
 
 
-def make_connection(start):
+def make_connection(start, total_companies, given_relation):
     queue = deque()
     queue.append(start)
     connection = 1
@@ -26,7 +30,10 @@ def make_connection(start):
     return connection
 
 
-connections = [0]+[make_connection(idx) for idx in range(1, total_companies+1)]
-for idx in range(1, total_companies+1):
-    if connections[idx] == max(connections):
-        print(idx, end=' ')
+if __name__ == "__main__":
+    total_companies, given_relation = init_data()
+    connections = [0]+[make_connection(idx, total_companies, given_relation)
+                       for idx in range(1, total_companies+1)]
+    for idx in range(1, total_companies+1):
+        if connections[idx] == max(connections):
+            print(idx, end=' ')
