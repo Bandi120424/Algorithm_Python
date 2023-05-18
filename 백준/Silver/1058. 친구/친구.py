8677082a9a86b8bf3b640e47e585d35bfb9e7933
N_Han <hny0309@gmail.com>
2023-05-18 09:03:29 +0900

    [Silver II] Title: 친구, Time: 72 ms, Memory: 34160 KB -BaekjoonHub

---
+++ 백준/Silver/1058. 친구/친구.py
@@ -0,0 +1,47 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+# def init_data():
+total_people = int(input())
+relation = [[1 if x == 'Y' else 0 for x in input().strip()]
+            for _ in range(total_people)]
+
+
+def find_connection(relation):
+    connection = [[] for _ in range(total_people)]
+    for i in range(total_people):
+        for j in range(i, total_people):
+            if relation[i][j] == True:
+                connection[i].append(j)
+                connection[j].append(i)
+
+    return connection
+
+
+def update_connection(start, connection):
+    queue = deque()
+    queue.append([start, 0])
+    visited = [False]*total_people
+    visited[start] = True
+    friend_count = 0
+
+    while queue:
+        person, ctn = queue.popleft()
+        if ctn > 1:
+            continue
+
+        for friend in connection[person]:
+            if visited[friend] == False and relation[person][friend]:
+                friend_count += 1
+                visited[friend] = True
+                queue.append([friend, ctn+1])
+    return friend_count
+
+
+max_friend = 0
+connection = find_connection(relation)
+for i in range(total_people):
+    max_friend = max(max_friend, update_connection(i, connection))
+
+print(max_friend)
