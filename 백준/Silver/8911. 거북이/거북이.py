3a7245c96bb0b4f3acb43343d004f28e3875d1c4
N_Han <hny0309@gmail.com>
2023-05-26 08:56:56 +0900

    [Silver III] Title: 거북이, Time: 364 ms, Memory: 120112 KB -BaekjoonHub

--- 백준/Silver/8911. 거북이/거북이.py
+++ 백준/Silver/8911. 거북이/거북이.py
@@ -1,48 +1,43 @@
 import sys
 input = sys.stdin.readline
 
-
-def init_data():
-    cases_num = int(input())
-    total_cases = [input() for _ in range(cases_num)]
-    return total_cases
-
-
+T = int(input())
+cases = [input() for _ in range(T)]
+#direction = N:0, E:1, S:2, W:3 
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+ 
 def move(pos, cur_dir, order):
-    DXS, DYS = [0, 1, 0, -1], [1, 0, -1, 0]  # direction = N:0, E:1, S:2, W:3
     x, y = pos[0], pos[1]
     if order == 'F':
-        return [x+DXS[cur_dir], y+DYS[cur_dir]]
-    else:  # order == 'B'
-        return [x+DXS[(cur_dir+2) % 4], y+DYS[(cur_dir+2) % 4]]
-
+        return [x+dx[cur_dir], y+dy[cur_dir]]
+    else: #order == 'B'
+        return [x+dx[(cur_dir+2)%4], y+dy[(cur_dir+2)%4]]
 
 def change_dir(cur_dir, order):
     if order == 'L':
-        return (cur_dir - 1) % 4
-    else:  # order == 'R'
-        return (cur_dir + 1) % 4
-
-
-def find_area(orders):
+        return (cur_dir - 1)%4
+    else: #order == 'R'
+        return (cur_dir + 1)%4
+        
+    
+for i in range(T):
     path = [[0, 0]]
-    dir = 0
-    for odr in orders:
-        if odr in {'F', 'B'}:
-            path.append(move(path[-1], dir, odr))
-        if odr in {'L', 'R'}:
-            dir = change_dir(dir, odr)
-
+    j, dir = 0, 0
+    for s in cases[i]:
+        if s in {'F', 'B'}:
+            path.append(move(path[j], dir, s))
+            j += 1
+        else: 
+            dir = change_dir(dir, s)
+    
     path_x = [i[0] for i in path]
     path_y = [i[1] for i in path]
-
+    
     width = max(path_x) - min(path_x)
     height = max(path_y) - min(path_y)
-
-    return width*height
-
-
-if __name__ == '__main__':
-    total_cases = init_data()
-    for case in total_cases:
-        print(find_area(case))
+    
+    print(width*height)
+    
+        
+        
+    
