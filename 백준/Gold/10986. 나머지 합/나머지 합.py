485127dcd84c833a8d72d16f98079d4be4f5baf6
N_Han <hny0309@gmail.com>
2023-06-04 23:59:50 +0900

    [Gold III] Title: 나머지 합, Time: 488 ms, Memory: 262472 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.py
+++ 백준/Gold/10986. 나머지 합/나머지 합.py
@@ -1,19 +1,28 @@
 import sys
 input = sys.stdin.readline
 
+def init_data():
+    _, modulo = map(int, input().split())
+    numbers = list(map(int, input().split()))
+    return modulo, numbers
 
-total_num, modulo = map(int, input().split())
-numbers = list(map(int, input().split()))
-remainder_info = {i:0 for i in range(modulo)}
-remainder_info[0] = 1
+def remainder_info(modulo, numbers):
+    remainder_inf = {i:0 for i in range(modulo)}
+    remainder_inf[0] = 1
 
-n_sum = 0
-for num in numbers:
-    n_sum += num
-    remainder_info[n_sum%modulo] += 1
+    n_sum = 0
+    for num in numbers:
+        n_sum += num
+        remainder_inf[n_sum%modulo] += 1
+    
+    return remainder_inf
 
-cases_ctn = 0
-for i in range(modulo):
-    cases_ctn += remainder_info[i]*(remainder_info[i]-1)//2
+if __name__ == '__main__':
+    modulo, numbers = init_data()
+    remainder_inf = remainder_info(modulo, numbers)
+    
+    cases_ctn = 0
+    for i in range(modulo):
+        cases_ctn += remainder_inf[i]*(remainder_inf[i]-1)//2
 
-print(cases_ctn)
+    print(cases_ctn)
