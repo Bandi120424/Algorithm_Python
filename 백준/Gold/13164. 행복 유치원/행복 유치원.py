432ea526df2d3c8c41720446859aa5a16299755b
N_Han <hny0309@gmail.com>
2023-06-16 09:21:32 +0900

    [Gold V] Title: 행복 유치원, Time: 240 ms, Memory: 66736 KB -BaekjoonHub

---
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+
+def init_data():
+    kids, teams = map(int, input().split())
+    heights = list(map(int, input().split()))
+
+    return teams, heights
+
+# 간격이 큰 두 사람을 최대한 분리
+def cost(teams, heights):
+    intervals = []
+    for i in range(1, len(heights)):
+        intervals.append(heights[i]-heights[i-1])
+    intervals.sort(reverse=True)
+
+    return sum(intervals[teams-1:])
+
+if __name__ == '__main__':
+    teams, heights = init_data()
+    print(cost(teams, heights))
