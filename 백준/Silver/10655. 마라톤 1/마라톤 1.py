8f576fa46b545a19f696637f1a80faead1afe530
N_Han <hny0309@gmail.com>
2023-06-30 09:04:09 +0900

    [Silver III] Title: 마라톤 1, Time: 244 ms, Memory: 55108 KB -BaekjoonHub

---
+++ 백준/Silver/10655. 마라톤 1/마라톤 1.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+
+def init_data():
+    total_checkpoints = int(input())
+    checkpoints = [list(map(int, input().split()))
+                   for _ in range(total_checkpoints)]
+
+    return total_checkpoints, checkpoints
+
+
+def manhattan_distance(pos1, pos2):
+    return abs(pos1[0]-pos2[0]) + abs(pos1[1]-pos2[1])
+
+
+def min_distance(total_checkpoints, checkpoints):
+
+    intervals = [0]+[manhattan_distance(checkpoints[i], checkpoints[i-1])
+                     for i in range(1, total_checkpoints)]
+    origin_distance = sum(intervals)
+
+    min_dist = origin_distance
+    for j in range(1, total_checkpoints-1):
+        min_dist = min(min_dist, origin_distance -
+                       intervals[j] - intervals[j+1]+manhattan_distance(checkpoints[j+1], checkpoints[j-1]))
+
+    return min_dist
+
+
+if __name__ == '__main__':
+    total_checkpoints, checkpoints = init_data()
+    print(min_distance(total_checkpoints, checkpoints))
