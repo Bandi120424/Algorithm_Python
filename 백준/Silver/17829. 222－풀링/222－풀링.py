8814bd1187d8a707670326c0e07872c054789df4
N_Han <hny0309@gmail.com>
2023-08-01 10:54:31 +0900

    [Silver II] Title: 222-풀링, Time: 532 ms, Memory: 75704 KB -BaekjoonHub

---
+++ 백준/Silver/17829. 222－풀링/222－풀링.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+
+
+def init_data():
+    size = int(input())
+
+    matrix = []
+    for _ in range(size):
+        row = list(map(int, input().split()))
+        matrix.append(row)
+
+    return size, matrix
+
+
+def find_second_component(numbers):
+    numbers.sort(reverse=True)
+    return numbers[1]
+
+
+def pooling(pool_size, size, matrix):
+    new_matrix = []
+    for i in range(size//pool_size):
+        row = []
+        for j in range(size//pool_size):
+            row.append(find_second_component([matrix[pool_size*i][pool_size*j], matrix[pool_size*i][pool_size*j+1],
+                                              matrix[pool_size*i+1][pool_size*j], matrix[pool_size*i+1][pool_size*j+1]]))
+        new_matrix.append(row)
+
+    return new_matrix
+
+
+if __name__ == '__main__':
+    size, matrix = init_data()
+    new_matrix = pooling(2, size, matrix)
+    while len(new_matrix) > 1:
+        new_matrix = pooling(2, len(new_matrix), new_matrix)
+
+    print(*new_matrix[0])
