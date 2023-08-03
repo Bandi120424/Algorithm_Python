5a8fbb4ed1a7c104ffecb877e4c6f868c6030a35
N_Han <hny0309@gmail.com>
2023-08-03 14:09:57 +0900

    [Silver I] Title: 작업, Time: 272 ms, Memory: 48796 KB -BaekjoonHub

---
+++ 백준/Silver/21937. 작업/작업.py
@@ -0,0 +1,47 @@
+import sys
+input = sys.stdin.readline
+
+
+class flowchart():
+    def __init__(self, num_of_work: int = 0, chart=None) -> None:
+        self.num_of_work = num_of_work
+        self.chart = chart
+
+
+def init_data():
+    num_of_work, work_order = map(int, input().split())
+    work_info = [[] for _ in range(num_of_work+1)]
+    for _ in range(work_order):
+        pre, post = map(int, input().split())
+        work_info[post].append(pre)
+    target = int(input())
+
+    return flowchart(num_of_work, work_info), target
+
+
+def minimum_workload(work_chart=None, target: int = 0) -> int:
+
+    if work_chart == None:
+        raise Exception("There is no flowchart")
+    if target == 0:
+        raise Exception("The target is wrong")
+
+    visited = [False]*(1+work_chart.num_of_work)
+    visited[target] = True
+    chart = work_chart.chart
+    stack = [target]
+    cnt = 0
+    while stack:
+        node = stack.pop()
+        for next_node in chart[node]:
+            if visited[next_node] == False:
+                visited[next_node] = True
+                cnt += 1
+                stack.append(next_node)
+
+    return cnt
+
+
+if __name__ == "__main__":
+    work_chart, target = init_data()
+    print(minimum_workload(work_chart, target))
