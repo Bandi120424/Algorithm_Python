f4493fa96e0fa6a8d17e6638a0d97546f7b0d84f
N_Han <hny0309@gmail.com>
2023-08-08 14:32:26 +0900

    [Silver I] Title: 동물원, Time: 72 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.py
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -3,21 +3,23 @@ input = sys.stdin.readline
 
 
 class Cage():
-    def __init__(self, size) -> None:
+    def __init__(self, size: int = 1) -> None:
         self.size = size
+        self.capture = [0]*(self.size+1)
+        self.capture[0] = 1
+        self.capture[1] = 3
 
     def construct_cage(self):
-        capture = [0]*(self.size+1)
-        capture[0] = 1
-        capture[1] = 3
         prev_cases = 0
         for i in range(2, self.size+1):
-            prev_cases += capture[i-2]
-            capture[i] = (capture[i-1] + 2*(1+prev_cases)) % 9901
+            prev_cases += self.capture[i-2]
+            self.capture[i] = (self.capture[i-1] + 2*(1+prev_cases)) % 9901
 
-        return capture[self.size]
+    def ways_to_capture(self, idx: int = 1):
+        return self.capture[idx]
 
 
 if __name__ == "__main__":
     lion_cage = Cage(int(input()))
-    print(lion_cage.construct_cage())
+    lion_cage.construct_cage()
+    print(lion_cage.ways_to_capture(lion_cage.size))
