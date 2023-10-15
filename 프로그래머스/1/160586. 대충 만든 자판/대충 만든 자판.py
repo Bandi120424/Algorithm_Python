c642b6114f669bf4e0da25276bcb49d545d4d3a2
N_Han <hny0309@gmail.com>
2023-10-15 23:21:53 +0900

    Refactor

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -8,15 +8,18 @@ def keymap_dict(keymap):
                 key_dict[keys[idx]] = min(key_dict[keys[idx]], idx+1)
     return key_dict
 
+def count_keystroke(key_dict, target):
+    key_stroke = 0
+    for c in target:
+        if c not in key_dict.keys():
+            return -1 
+        key_stroke += key_dict[c]
+        
+    return key_stroke 
+    
 def solution(keymap, targets):
     answer = []
     key_dict = keymap_dict(keymap)
     for target_str in targets:
-        key_stroke = 0
-        for c in target_str:
-            if c not in key_dict.keys():
-                key_stroke = -1
-                break 
-            key_stroke += key_dict[c]
-        answer.append(key_stroke)
+        answer.append(count_keystroke(key_dict, target_str))
     return answer
